c542dada6589c115f0f1546f0d38c2e584403ac9
Sebastian <sebastian@snowtrack.io>
2021-02-14 21:57:50 -0500

    set timeout

--- test/10.benchmark.ts
+++ test/10.benchmark.ts
@@ -4,6 +4,7 @@ import * as os from 'os';
 import { snowFsAddTexture, snowFsRestoreTexture, snowFsRmTexture } from '../benchmarks/snowfs-vs-git';
 
 test('run benchmark', async (t) => {
+  t.timeout(180000);
   const playground = os.tmpdir();
   const gitPath = join(playground, 'snowfs-benchmark');
   await snowFsAddTexture(gitPath, t.log);
