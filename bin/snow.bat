dc76d39d6d306e628ff0b0ecfbeeebcee852b628
Sebastian <sebastian@snowtrack.io>
2021-02-14 21:10:59 -0500

    add bin

--- bin/snow.bat
+++ bin/snow.bat
@@ -2,7 +2,7 @@
 SETLOCAL 
 CALL :find_dp0 
 echo %dp0%
-IF NOT EXIST "%dp0%\..\node_modules" ( 
+IF NOT EXIST "%dp0%..\node_modules" ( 
   echo "node_modules missing - you must first install all node packages by executing 'npm install' in the root directory of SnowFS" 
   EXIT /b 
 ) 
