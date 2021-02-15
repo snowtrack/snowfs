174a5906216b767a901723081f87df6841780fe3
Sebastian <sebastian@snowtrack.io>
2021-02-14 21:15:49 -0500

    add bin

--- bin/snow.bat
+++ bin/snow.bat
@@ -1,14 +1,10 @@
 @ECHO OFF 
 SETLOCAL 
 CALL :find_dp0 
-echo %dp0%
-IF NOT EXIST "%dp0%..\node_modules" ( 
-  echo "node_modules missing - you must first install all node packages by executing 'npm install' in the root directory of SnowFS" 
-  EXIT /b 
-) 
+
  
-IF EXIST "%dp0%\node.exe" ( 
-  SET "_prog=%dp0%\node.exe" 
+IF EXIST "%dp0%node.exe" ( 
+  SET "_prog=%dp0%node.exe" 
 ) ELSE ( 
   SET "_prog=node" 
   SET PATHEXT=%PATHEXT:;.JS;=;% 
