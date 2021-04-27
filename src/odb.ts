32f9069cdf23106c552b3fa2b4b1b4729cad92b3
Sebastian Rath <sebastian@snowtrack.io>
2021-04-27 17:51:17 -0400

    :star: Keep file extensions for files in odb (Closes: 158)

--- src/odb.ts
+++ src/odb.ts
@@ -2,7 +2,7 @@ import * as crypto from 'crypto';
 import * as fse from 'fs-extra';
 
 import {
-  basename, join, dirname, relative,
+  basename, join, dirname, relative, extname,
 } from './path';
 import {
   DirItem, OSWALK, osWalk, zipFile,
@@ -171,9 +171,9 @@ export class Odb {
     });
   }
 
-  getObjectPath(file: TreeFile): string {
+  getAbsObjectPath(file: TreeFile): string {
     const objects: string = join(this.repo.options.commondir, 'objects');
-    return join(objects, file.hash.substr(0, 2), file.hash.substr(2, 2), file.hash.toString());
+    return join(objects, file.hash.substr(0, 2), file.hash.substr(2, 2), file.hash.toString() + extname(file.path));
   }
 
   writeReference(ref: Reference): Promise<void> {
@@ -236,7 +236,7 @@ export class Odb {
     const tmpFilename: string = crypto.createHash('sha256').update(process.hrtime().toString()).digest('hex');
     const objects: string = join(this.repo.options.commondir, 'objects');
     const tmpDir: string = join(this.repo.options.commondir, 'tmp');
-    const tmpPath: string = join(tmpDir, tmpFilename.toString());
+    const tmpPath: string = join(tmpDir, tmpFilename);
 
     let dstFile: string;
     let filehash: string;
@@ -250,7 +250,7 @@ export class Odb {
       .then((res: {filehash: string, hashBlocks?: HashBlock[]}) => {
         filehash = res.filehash;
         hashBlocks = res.hashBlocks;
-        dstFile = join(objects, filehash.substr(0, 2), filehash.substr(2, 2), filehash.toString());
+        dstFile = join(objects, filehash.substr(0, 2), filehash.substr(2, 2), filehash.toString() + extname(filepath));
         return fse.pathExists(dstFile);
       })
       .then((exists: boolean) => {
@@ -276,6 +276,7 @@ export class Odb {
         .then((stat: fse.Stats) => ({
           file: relative(this.repo.repoWorkDir, filepath),
           fileinfo: {
+            ext: extname(filepath),
             hash: filehash,
             stat: {
               size: stat.size,
@@ -288,8 +289,9 @@ export class Odb {
       .then((res) => this.repo.modified(res));
   }
 
-  readObject(hash: string, dstAbsPath: string, ioContext: IoContext): Promise<void> {
-    const objectFile: string = join(this.repo.options.commondir, 'objects', hash.substr(0, 2), hash.substr(2, 2), hash.toString());
+  readObject(file: TreeFile, dstAbsPath: string, ioContext: IoContext): Promise<void> {
+    const hash: string = file.hash;
+    const objectFile: string = this.getAbsObjectPath(file);
 
     return fse.pathExists(objectFile).then((exists: boolean) => {
       if (!exists) {
