35a2e5100d4885ddfe754effcf7f6af47215aaab
Sebastian Rath <sebastian@snowtrack.io>
2021-05-09 17:36:48 -0400

    readdir and stat were used before they were defined

--- src/io.ts
+++ src/io.ts
@@ -66,98 +66,6 @@ export enum OSWALK {
   NO_RECURSIVE = 16
 }
 
-/**
- * Helper function to recursively request information of all files or directories of a given directory.
- * @param dirPath       The directory in question.
- * @param request       Specify which elements are of interest.
- * @param dirItemRef    Only for internal use, must be not set when called.
- */
-export function osWalk(dirPath: string, request: OSWALK): Promise<DirItem[]> {
-  const returnDirs = request & OSWALK.DIRS;
-  const returnFiles = request & OSWALK.FILES;
-  const returnHidden = request & OSWALK.HIDDEN;
-  const browseRepo = request & OSWALK.BROWSE_REPOS;
-
-  function internalOsWalk(dirPath: string, request: OSWALK, relPath: string, dirItemRef?: DirItem): Promise<DirItem[]> {
-    if (dirPath.endsWith('/')) {
-      // if directory ends with a seperator, we cut it off to ensure
-      // we don't return a path like /foo/directory//file.jpg
-      dirPath = dirPath.substr(0, dirPath.length - 1);
-    }
-
-    const dirItems = [];
-    const dirItemsTmp: DirItem[] = [];
-    return new Promise<string[]>((resolve, reject) => {
-      readdir(dirPath, (error, entries: string[]) => {
-        if (error) {
-          // While browsing through a sub-directory, readdir
-          // might fail if the directory e.g. gets deleted at the same
-          // time. Therefore sub-directories don't throw an error
-          if (dirItemRef) {
-            resolve([]);
-          } else {
-            reject(error);
-          }
-          return;
-        }
-
-        // normalize all dir items
-        resolve(entries.map(normalize));
-      });
-    })
-      .then((items: string[]) => {
-        const promises = [];
-
-        for (const item of items) {
-          if (item === '.DS_Store' || item === 'thumbs.db') {
-            continue;
-          } else if (!browseRepo && (item === '.snow' || item === '.git')) {
-            continue;
-          } else if (!returnHidden && item.startsWith('.')) {
-            continue;
-          }
-
-          const absPath = `${dirPath}/${item}`;
-
-          const stats = stat(absPath);
-          promises.push(stats);
-          dirItemsTmp.push({
-            absPath,
-            isempty: false,
-            relPath: relPath.length === 0 ? item : `${relPath}/${item}`,
-            stats: null, // not resolved yet.. (*)
-          });
-        }
-
-        return Promise.all(promises);
-      }).then((itemStatArray: Stats[]) => {
-        const promises = [];
-        let i = 0;
-        for (const itemStats of itemStatArray) {
-          const dirItem = dirItemsTmp[i++];
-          dirItem.stats = itemStats; // (*)...update stats which are now resolved
-
-          if ((itemStats.isDirectory() && returnDirs) || (!itemStats.isDirectory() && returnFiles)) {
-            dirItems.push(dirItem);
-          }
-
-          if (itemStats.isDirectory() && !(request & OSWALK.NO_RECURSIVE)) {
-            promises.push(internalOsWalk(dirItem.absPath, request, dirItem.relPath, dirItem));
-          }
-        }
-
-        if (dirItemRef) {
-          dirItemRef.isempty = itemStatArray.length === 0;
-        }
-
-        return Promise.all(promises);
-      })
-      .then((dirItemResults: DirItem[]) => dirItems.concat(...dirItemResults));
-  }
-
-  return internalOsWalk(dirPath, request, '');
-}
-
 function darwinZip(src: string, dst: string): Promise<void> {
   const p0 = cp.spawn('ditto', ['-c', '-k', '--sequesterRsrc', src, dst]);
   return new Promise((resolve, reject) => {
@@ -295,4 +203,96 @@ export function createReadStream(path: PathLike, options?: string | {
   highWaterMark?: number;
 }): fse.ReadStream {
   return fs.createReadStream(path, options);
+}
+
+/**
+ * Helper function to recursively request information of all files or directories of a given directory.
+ * @param dirPath       The directory in question.
+ * @param request       Specify which elements are of interest.
+ * @param dirItemRef    Only for internal use, must be not set when called.
+ */
+export function osWalk(dirPath: string, request: OSWALK): Promise<DirItem[]> {
+  const returnDirs = request & OSWALK.DIRS;
+  const returnFiles = request & OSWALK.FILES;
+  const returnHidden = request & OSWALK.HIDDEN;
+  const browseRepo = request & OSWALK.BROWSE_REPOS;
+
+  function internalOsWalk(dirPath: string, request: OSWALK, relPath: string, dirItemRef?: DirItem): Promise<DirItem[]> {
+    if (dirPath.endsWith('/')) {
+      // if directory ends with a seperator, we cut it off to ensure
+      // we don't return a path like /foo/directory//file.jpg
+      dirPath = dirPath.substr(0, dirPath.length - 1);
+    }
+
+    const dirItems = [];
+    const dirItemsTmp: DirItem[] = [];
+    return new Promise<string[]>((resolve, reject) => {
+      readdir(dirPath, (error, entries: string[]) => {
+        if (error) {
+          // While browsing through a sub-directory, readdir
+          // might fail if the directory e.g. gets deleted at the same
+          // time. Therefore sub-directories don't throw an error
+          if (dirItemRef) {
+            resolve([]);
+          } else {
+            reject(error);
+          }
+          return;
+        }
+
+        // normalize all dir items
+        resolve(entries.map(normalize));
+      });
+    })
+      .then((items: string[]) => {
+        const promises = [];
+
+        for (const item of items) {
+          if (item === '.DS_Store' || item === 'thumbs.db') {
+            continue;
+          } else if (!browseRepo && (item === '.snow' || item === '.git')) {
+            continue;
+          } else if (!returnHidden && item.startsWith('.')) {
+            continue;
+          }
+
+          const absPath = `${dirPath}/${item}`;
+
+          const stats = stat(absPath);
+          promises.push(stats);
+          dirItemsTmp.push({
+            absPath,
+            isempty: false,
+            relPath: relPath.length === 0 ? item : `${relPath}/${item}`,
+            stats: null, // not resolved yet.. (*)
+          });
+        }
+
+        return Promise.all(promises);
+      }).then((itemStatArray: Stats[]) => {
+        const promises = [];
+        let i = 0;
+        for (const itemStats of itemStatArray) {
+          const dirItem = dirItemsTmp[i++];
+          dirItem.stats = itemStats; // (*)...update stats which are now resolved
+
+          if ((itemStats.isDirectory() && returnDirs) || (!itemStats.isDirectory() && returnFiles)) {
+            dirItems.push(dirItem);
+          }
+
+          if (itemStats.isDirectory() && !(request & OSWALK.NO_RECURSIVE)) {
+            promises.push(internalOsWalk(dirItem.absPath, request, dirItem.relPath, dirItem));
+          }
+        }
+
+        if (dirItemRef) {
+          dirItemRef.isempty = itemStatArray.length === 0;
+        }
+
+        return Promise.all(promises);
+      })
+      .then((dirItemResults: DirItem[]) => dirItems.concat(...dirItemResults));
+  }
+
+  return internalOsWalk(dirPath, request, '');
 }
