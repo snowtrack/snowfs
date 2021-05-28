eed5eaeebec8e17b4212d71cb6d80fc82a0df017
Sebastian <sebastian@snowtrack.io>
2021-05-27 20:39:24 -0400

    index files and commit trees are now merged

--- src/treedir.ts
+++ src/treedir.ts
@@ -2,15 +2,18 @@
 /* eslint-disable no-useless-constructor */
 import * as fse from 'fs-extra';
 import * as crypto from 'crypto';
+import { unionWith } from 'lodash';
 import * as io from './io';
 
 import {
-  join, relative, normalize, extname,
+  join, relative, normalize, extname, dirname, basename,
 } from './path';
 import { Repository } from './repository';
 import {
-  FileInfo, getPartHash, HashBlock, MB20, StatsSubset,
+  getPartHash, HashBlock, MB20, StatsSubset,
 } from './common';
+
+const sortPaths = require('sort-paths');
 
 export const enum FILEMODE {
   UNREADABLE = 0,
@@ -56,7 +59,38 @@ export const enum DETECTIONMODE {
   SIZE_AND_HASH_FOR_ALL_FILES = 4
 }
 
-export class TreeEntry {
+export function calculateSizeAndHash(items: TreeEntry[]): [number, string] {
+  const hash = crypto.createHash('sha256');
+  let size = 0;
+
+  // Here we ensure that the hash of the tree entries is not dependend on their order
+  items = sortPaths(items, (item) => item.path, '/');
+
+  for (const r of items) {
+    size += r.stats.size;
+    hash.update(r.hash.toString());
+  }
+  return [size, hash.digest('hex')];
+}
+
+function generateSizeAndCaches(item: TreeEntry): [number, string] {
+  if (item instanceof TreeDir) {
+    for (const subitem of item.children) {
+      if (subitem instanceof TreeDir) {
+        generateSizeAndCaches(subitem);
+      }
+    }
+
+    const calcs = calculateSizeAndHash(item.children);
+    item.stats.size = calcs[0];
+    item.hash = calcs[1];
+    return [calcs[0], calcs[1]];
+  }
+
+  return [item.stats.size, item.hash];
+}
+
+export abstract class TreeEntry {
   constructor(
     public hash: string,
     public path: string,
@@ -71,6 +105,8 @@ export class TreeEntry {
   isFile(): boolean {
     return this instanceof TreeFile;
   }
+
+  abstract clone(parent?: TreeDir);
 }
 
 export class TreeFile extends TreeEntry {
@@ -84,7 +120,12 @@ export class TreeFile extends TreeEntry {
     super(hash, path, stats);
   }
 
-  toString(): string {
+  clone(parent?: TreeDir): TreeFile {
+    return new TreeFile(this.hash,
+      this.path, StatsSubset.clone(this.stats), this.ext, parent);
+  }
+
+  toJsonObject(): any {
     if (!this.parent && this.path) {
       throw new Error('parent has no path');
     } else if (this.parent && !this.path) {
@@ -119,7 +160,7 @@ export class TreeFile extends TreeEntry {
       // microseconds. That's why all items are identified if the mtime from the commit
       // and the mtime from the file on disk is greater than 1ms, everything below is
       // considered equal.
-      if (Math.abs(this.stats.mtimeMs - newStats.mtimeMs) >= 1.0) {
+      if (Math.abs(+this.stats.mtime - (+newStats.mtime)) >= 1.0) {
         switch (detectionMode) {
           case DETECTIONMODE.DEFAULT: {
             const ext = extname(filepath);
@@ -171,10 +212,53 @@ export class TreeDir extends TreeEntry {
   }
 
   static createRootTree(): TreeDir {
-    return new TreeDir('', { size: 0, ctimeMs: 0, mtimeMs: 0 });
+    return new TreeDir('', { size: 0, ctime: new Date(0), mtime: new Date(0) });
   }
 
-  toString(includeChildren?: boolean): string {
+  clone(parent?: TreeDir): TreeDir {
+    const newTree = new TreeDir(this.path, StatsSubset.clone(this.stats), parent);
+    newTree.children = this.children.map((c: TreeEntry) => c.clone(newTree));
+    return newTree;
+  }
+
+  /**
+   * Merge two trees, with target having the precedence in case
+   * the element is already located in 'source.
+   */
+  static merge(source: TreeEntry, target: TreeEntry) {
+    function privateMerge(source: TreeEntry, target: TreeEntry) {
+      // walk source nodes...
+      if (source instanceof TreeDir && target instanceof TreeDir) {
+        const newItems = new Map<string, TreeEntry>();
+        for (const child of source.children) {
+          newItems.set(child.path, child);
+        }
+
+        for (const child of target.children) {
+          newItems.set(child.path, child);
+        }
+
+        for (const sourceItem of source.children) {
+          for (const targetItem of target.children) {
+            if (targetItem.path === sourceItem.path) {
+              const res = privateMerge(sourceItem, targetItem);
+              newItems.set(res.path, res);
+            }
+          }
+        }
+        target.children = Array.from(newItems.values());
+
+        const calcs = generateSizeAndCaches(target);
+        target.stats.size = calcs[0];
+        target.hash = calcs[1];
+      }
+      return target;
+    }
+
+    return privateMerge(source, target.clone()) as TreeDir;
+  }
+
+  toJsonObject(includeChildren?: boolean): any {
     if (!this.parent && this.path) {
       throw new Error('parent has no path');
     } else if (this.parent && (!this.path || this.path.length === 0)) {
@@ -182,9 +266,17 @@ export class TreeDir extends TreeEntry {
       throw new Error('item must have path');
     }
 
-    const children: string[] = this.children.map((value: TreeDir | TreeFile) => value.toString(includeChildren));
-    const stats = JSON.stringify(this.stats);
-    return `{"hash": "${this.hash.toString()}", "path": "${this.path ?? ''}", "stats": ${stats}, "children": [${children.join(',')}]}`;
+    const children: string[] = this.children.map((value: TreeDir | TreeFile) => value.toJsonObject(includeChildren));
+
+    const stats: any = {
+      size: this.stats.size,
+      ctime: this.stats.ctime.getTime(),
+      mtime: this.stats.mtime.getTime(),
+    };
+
+    return {
+      hash: this.hash, path: this.path ?? '', stats, children,
+    };
   }
 
   getAllTreeFiles(opt: {entireHierarchy: boolean, includeDirs: boolean}): Map<string, TreeEntry> {
@@ -196,7 +288,7 @@ export class TreeDir extends TreeEntry {
         if (opt.includeDirs) {
           map.set(obj.path, obj);
         }
-        return (obj as TreeDir).children.forEach((c: any) => visit(c, map));
+        return obj.children.forEach((c: any) => visit(c, map));
       }
       map.set(obj.path, obj);
     };
@@ -216,6 +308,10 @@ export class TreeDir extends TreeEntry {
   }
 
   find(relativePath: string): TreeEntry | null {
+    if (relativePath === this.path) {
+      return this;
+    }
+
     let tree: TreeEntry | null = null;
     // TODO: (Seb) return faster if found
     TreeDir.walk(this, (entry: TreeDir | TreeFile) => {
@@ -227,36 +323,16 @@ export class TreeDir extends TreeEntry {
   }
 
   /**
-   * Browse through the entire hierarchy of the tree and remove the given file.
-   * Doesn't throw an error if the element is not found.
-   *
-   * @param relativePath      The relative file path to remove.
+   * Browse through the entire hierarchy of the tree and remove the given item.
    */
-  remove(relativePath: string): void {
-    function privateDelete(
-      tree: TreeDir,
-      cb: (entry: TreeEntry, index: number, length: number) => boolean,
-    ) {
-      let i = 0;
-
-      for (const entry of tree.children) {
-        if (cb(entry, i, tree.children.length)) {
-          tree.children.splice(i, 1);
-          return;
-        }
-        if (entry.isDirectory()) {
-          privateDelete(entry as TreeDir, cb);
-        }
-        i++;
+  static remove(tree: TreeDir,
+    cb: (entry: TreeEntry, index: number, array: TreeEntry[]) => boolean): void {
+    for (const child of tree.children) {
+      if (child instanceof TreeDir) {
+        TreeDir.remove(child, cb);
       }
     }
-
-    // TODO: (Seb) return faster if found
-    privateDelete(this, (entry: TreeEntry): boolean => {
-      if (entry.path === relativePath) {
-        return true;
-      }
-    });
+    tree.children = tree.children.filter((value: TreeEntry, index: number, array: TreeEntry[]) => !cb(value, index, array));
   }
 
   static walk(
@@ -266,8 +342,8 @@ export class TreeDir extends TreeEntry {
     let i = 0;
     for (const entry of tree.children) {
       cb(<TreeFile>entry, i, tree.children.length);
-      if (entry.isDirectory()) {
-        TreeDir.walk(entry as TreeDir, cb);
+      if (entry instanceof TreeDir) {
+        TreeDir.walk(entry, cb);
       }
       i++;
     }
@@ -276,7 +352,6 @@ export class TreeDir extends TreeEntry {
 // This function has the same basic functioanlity as io.osWalk(..) but works with Tree
 export function constructTree(
   dirPath: string,
-  processed: Map<string, FileInfo>,
   tree?: TreeDir,
   root?: string,
 ): Promise<TreeDir> {
@@ -307,39 +382,24 @@ export function constructTree(
       const promises: Promise<any>[] = [];
 
       for (const entry of entries) {
-        if (entry === '.snow' || entry === '.git') {
+        if (entry === '.snow' || entry === '.git' || entry === '.DS_Store' || entry === 'thumbs.db') {
           continue;
         }
 
         const absPath = `${dirPath}/${entry}`;
+        const relPath = relative(root, absPath);
         promises.push(
           io.stat(absPath).then((stat: fse.Stats) => {
             if (stat.isDirectory()) {
-              const subtree: TreeDir = new TreeDir(
-                relative(root, absPath),
-                {
-                  ctimeMs: stat.ctimeMs,
-                  mtimeMs: stat.mtimeMs,
-                  size: stat.size,
-                },
-                tree,
-              );
+              // hash is later added to subtree (see next promise task)
+              const subtree: TreeDir = new TreeDir(relative(root, absPath), StatsSubset.clone(stat), tree);
+
               tree.children.push(subtree);
-              return constructTree(absPath, processed, subtree, root);
+              return constructTree(absPath, subtree, root);
             }
-            const fileinfo: FileInfo | null = processed?.get(relative(root, absPath));
-            if (fileinfo) {
-              const path: string = relative(root, absPath);
-              const entry: TreeFile = new TreeFile(fileinfo.hash,
-                path, {
-                  size: stat.size,
-                  ctimeMs: stat.ctimeMs,
-                  mtimeMs: stat.mtimeMs,
-                }, extname(path), tree);
-              tree.children.push(entry);
-            } else {
-              // console.warn(`No hash for ${absPath}`);
-            }
+
+            const entry: TreeFile = new TreeFile('', relPath, StatsSubset.clone(stat), extname(relPath), tree);
+            tree.children.push(entry);
           }),
         );
       }
@@ -347,16 +407,12 @@ export function constructTree(
       return Promise.all(promises);
     })
     .then(() => {
-      // assign the parent 'tree' a hash of all its children
-      // and calculate their size
-      const hash = crypto.createHash('sha256');
+      // calculate the size of the directory
       let size = 0;
       for (const r of tree.children) {
         size += r.stats.size;
-        hash.update(r.hash.toString());
       }
       tree.stats.size = size;
-      tree.hash = hash.digest('hex');
       return tree;
     });
 }
