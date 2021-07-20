6114a374eb6f186c02ef33a41b35c6feb692b064
Sebastian Rath <sebastian@snowtrack.io>
2021-07-19 23:59:44 -0400

    fix path order in tests

--- test/7.repo.diff.ts
+++ test/7.repo.diff.ts
@@ -9,6 +9,8 @@ import { COMMIT_ORDER, Repository } from '../src/repository';
 import { getRandomPath } from './helper';
 import { Commit } from '../src/commit';
 
+const sortPaths = require('sort-paths');
+
 function writeFileAndCommit(repo: Repository, filename: string, message: string): Promise<Commit> {
   return fse.writeFile(join(repo.workdir(), filename), message)
     .then(() => {
@@ -246,23 +248,27 @@ test('Diff with subdirectories', async (t) => {
     const diff = new Diff(commit1, commit0, { includeDirs: true });
 
     const addedArray = Array.from(diff.added());
-    t.is(addedArray.length, 9, 'expected 1 element: fooA.txt');
-    t.is(addedArray[0].path, 'fooA.txt');
-    t.is(addedArray[0].hash, 'fbb70f5f22a0f1040c9978351c8b3302fa0f77044e36d6ca0fdb0b646fc32611');
-    t.is(addedArray[1].path, 'subdir1');
-    t.is(addedArray[1].hash, '34f40a8f2533b51ed3cdc44d0913431f7b6642cea194eea982ff6aa268c85e48');
-    t.is(addedArray[2].path, 'subdir1/fooA.txt');
-    t.is(addedArray[2].hash, 'eb4d879649dbc97e1bcc280c1abcd56c30d211da2a0f3ec7e6251124c7646edd');
-    t.is(addedArray[3].path, 'subdir1/fooB.txt');
-    t.is(addedArray[3].hash, '81a281150cd97e24ce7628d98f90b1789676fbad19f6bb7c50676d41144b813c');
-    t.is(addedArray[4].path, 'subdir1/fooC.txt');
-    t.is(addedArray[4].hash, '1911748a6284c23df9f9619151a364c0d0fd9d05b3386a1b808fa7773f90c2ad');
-    t.is(addedArray[5].path, 'subdir1/fooD.txt');
-    t.is(addedArray[5].hash, '1baf798eba71b480295794fadf3e929b97c36001a985ccddffdddad445b21aa4');
-    t.is(addedArray[6].path, 'subdir2');
-    t.is(addedArray[6].hash, '86aee0487e56510ef47d0c6c369f08ba5585e57a251d9c39461519430bc358d4');
-    t.is(addedArray[8].path, 'subdir2/fooB.txt');
-    t.is(addedArray[8].hash, '26a9e4753ef2791e012bca321f9f9cacba992b752d9e38710e157d05396af65a');
+    const sortedAddedArray = sortPaths(addedArray, (item) => item.path, '/');
+
+    t.is(sortedAddedArray.length, 9, 'expected 1 element: fooA.txt');
+    t.is(sortedAddedArray[0].path, 'fooA.txt');
+    t.is(sortedAddedArray[0].hash, 'fbb70f5f22a0f1040c9978351c8b3302fa0f77044e36d6ca0fdb0b646fc32611');
+    t.is(sortedAddedArray[1].path, 'subdir1');
+    t.is(sortedAddedArray[1].hash, '34f40a8f2533b51ed3cdc44d0913431f7b6642cea194eea982ff6aa268c85e48');
+    t.is(sortedAddedArray[2].path, 'subdir2');
+    t.is(sortedAddedArray[2].hash, '86aee0487e56510ef47d0c6c369f08ba5585e57a251d9c39461519430bc358d4');
+    t.is(sortedAddedArray[3].path, 'subdir1/fooA.txt');
+    t.is(sortedAddedArray[3].hash, 'eb4d879649dbc97e1bcc280c1abcd56c30d211da2a0f3ec7e6251124c7646edd');
+    t.is(sortedAddedArray[4].path, 'subdir1/fooB.txt');
+    t.is(sortedAddedArray[4].hash, '81a281150cd97e24ce7628d98f90b1789676fbad19f6bb7c50676d41144b813c');
+    t.is(sortedAddedArray[5].path, 'subdir1/fooC.txt');
+    t.is(sortedAddedArray[5].hash, '1911748a6284c23df9f9619151a364c0d0fd9d05b3386a1b808fa7773f90c2ad');
+    t.is(sortedAddedArray[6].path, 'subdir1/fooD.txt');
+    t.is(sortedAddedArray[6].hash, '1baf798eba71b480295794fadf3e929b97c36001a985ccddffdddad445b21aa4');
+    t.is(sortedAddedArray[7].path, 'subdir2/fooA.txt');
+    t.is(sortedAddedArray[7].hash, '61ec705e763af9e3b2e5c12deba78cb11f51e429f8a4aa4160922d9a926bba16');
+    t.is(sortedAddedArray[8].path, 'subdir2/fooB.txt');
+    t.is(sortedAddedArray[8].hash, '26a9e4753ef2791e012bca321f9f9cacba992b752d9e38710e157d05396af65a');
 
     const modifiedArray = Array.from(diff.modified());
     t.is(modifiedArray.length, 0, 'expected 0 elements');
@@ -275,7 +281,7 @@ test('Diff with subdirectories', async (t) => {
   };
 
   const test1 = () => {
-    t.log('Compare initial commit with first commit');
+    t.log('Compare 2nd commit with first commit');
     const diff = new Diff(commit2, commit1, { includeDirs: true });
 
     const addedArray = Array.from(diff.added());
@@ -289,21 +295,21 @@ test('Diff with subdirectories', async (t) => {
     t.is(modifiedArray[1].hash, '0c6611c83f11f7dc822c64c3f1e934907bef406b802e6d7be979171743b25da2');
 
     const nonModifiedArray = Array.from(diff.nonModified());
-    t.is(nonModifiedArray.length, 7, 'expected 8 elements');
-    t.is(nonModifiedArray[0].path, 'fooA.txt');
-    t.is(nonModifiedArray[0].hash, 'fbb70f5f22a0f1040c9978351c8b3302fa0f77044e36d6ca0fdb0b646fc32611');
-    t.is(nonModifiedArray[1].path, 'subdir1');
-    t.is(nonModifiedArray[1].hash, '34f40a8f2533b51ed3cdc44d0913431f7b6642cea194eea982ff6aa268c85e48');
-    t.is(nonModifiedArray[2].path, 'subdir1/fooA.txt');
-    t.is(nonModifiedArray[2].hash, 'eb4d879649dbc97e1bcc280c1abcd56c30d211da2a0f3ec7e6251124c7646edd');
-    t.is(nonModifiedArray[3].path, 'subdir1/fooB.txt');
-    t.is(nonModifiedArray[3].hash, '81a281150cd97e24ce7628d98f90b1789676fbad19f6bb7c50676d41144b813c');
-    t.is(nonModifiedArray[4].path, 'subdir1/fooC.txt');
-    t.is(nonModifiedArray[4].hash, '1911748a6284c23df9f9619151a364c0d0fd9d05b3386a1b808fa7773f90c2ad');
-    t.is(nonModifiedArray[5].path, 'subdir1/fooD.txt');
-    t.is(nonModifiedArray[5].hash, '1baf798eba71b480295794fadf3e929b97c36001a985ccddffdddad445b21aa4');
-    t.is(nonModifiedArray[6].path, 'subdir2/fooB.txt');
-    t.is(nonModifiedArray[6].hash, '26a9e4753ef2791e012bca321f9f9cacba992b752d9e38710e157d05396af65a');
+    const sortedNonModifiedArray = sortPaths(nonModifiedArray, (item) => item.path, '/');
+    t.is(sortedNonModifiedArray[0].path, 'fooA.txt');
+    t.is(sortedNonModifiedArray[0].hash, 'fbb70f5f22a0f1040c9978351c8b3302fa0f77044e36d6ca0fdb0b646fc32611');
+    t.is(sortedNonModifiedArray[1].path, 'subdir1');
+    t.is(sortedNonModifiedArray[1].hash, '34f40a8f2533b51ed3cdc44d0913431f7b6642cea194eea982ff6aa268c85e48');
+    t.is(sortedNonModifiedArray[2].path, 'subdir1/fooA.txt');
+    t.is(sortedNonModifiedArray[2].hash, 'eb4d879649dbc97e1bcc280c1abcd56c30d211da2a0f3ec7e6251124c7646edd');
+    t.is(sortedNonModifiedArray[3].path, 'subdir1/fooB.txt');
+    t.is(sortedNonModifiedArray[3].hash, '81a281150cd97e24ce7628d98f90b1789676fbad19f6bb7c50676d41144b813c');
+    t.is(sortedNonModifiedArray[4].path, 'subdir1/fooC.txt');
+    t.is(sortedNonModifiedArray[4].hash, '1911748a6284c23df9f9619151a364c0d0fd9d05b3386a1b808fa7773f90c2ad');
+    t.is(sortedNonModifiedArray[5].path, 'subdir1/fooD.txt');
+    t.is(sortedNonModifiedArray[5].hash, '1baf798eba71b480295794fadf3e929b97c36001a985ccddffdddad445b21aa4');
+    t.is(sortedNonModifiedArray[6].path, 'subdir2/fooB.txt');
+    t.is(sortedNonModifiedArray[6].hash, '26a9e4753ef2791e012bca321f9f9cacba992b752d9e38710e157d05396af65a');
 
     const deletedArray = Array.from(diff.deleted());
     t.is(deletedArray.length, 0, 'expected 0 elements');
