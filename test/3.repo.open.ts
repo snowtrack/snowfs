f5a3821dd2baf96de6ef9c6fcf2215176548d75a
Sebastian Rath <sebastian@snowtrack.io>
2021-08-04 10:22:58 -0400

    add collision test

--- test/3.repo.open.ts
+++ test/3.repo.open.ts
@@ -88,3 +88,34 @@ test('repo open-commondir-inside-subdirectory', async (t) => {
     })
     .then(() => rmdir(repo.workdir()));
 });
+
+test('repo open-repo-workdir-commondir-collision-test', async (t) => {
+  /** This test ensures that Repository.initExt never overwrites another repo if executed on the same directory */
+  let workdir: string;
+  let commondir: string;
+
+  // create repo
+  workdir = getRandomPath();
+  await Repository.initExt(workdir);
+  // attempt to create repo at same location
+  const error1 = await t.throwsAsync(async () => Repository.initExt(workdir));
+  t.is(error1.message, 'workdir already exists');
+
+  // create repo
+  workdir = getRandomPath();
+  commondir = getRandomPath();
+  await Repository.initExt(workdir, { commondir });
+  // create repo at another location but with same commondir
+  workdir = getRandomPath();
+  const error2 = await t.throwsAsync(async () => Repository.initExt(workdir, { commondir }));
+  t.is(error2.message, 'commondir already exists');
+
+  // create repo
+  workdir = getRandomPath();
+  commondir = getRandomPath();
+  await Repository.initExt(workdir, { commondir });
+  // attempt to use different commondirs but same workdir
+  commondir = getRandomPath();
+  const error3 = await t.throwsAsync(async () => Repository.initExt(workdir, { commondir }));
+  t.is(error3.message, 'workdir already exists');
+});
