fd819ae342542adb97276db9212636bebb7b70e3
Sebastian Rath <sebastian@snowtrack.io>
2021-12-04 20:56:20 -0500

    seperate stdout from stderr

--- test/helper.ts
+++ test/helper.ts
@@ -90,29 +90,30 @@ export function exec(t, command: string, args?: string[], opts?: {cwd?: string},
 
   const p0 = spawn(command, args ?? [], { cwd: opts?.cwd ?? '.', env: Object.assign(process.env, { SUPPRESS_BANNER: 'true' }) });
   return new Promise((resolve, reject) => {
-    let std = '';
+    let stdout = '';
+    let stderr = '';
     if (stdiopts & EXEC_OPTIONS.WRITE_STDIN) {
       p0.stdin.write(`${stdin}\n`);
       p0.stdin.end(); /// this call seems necessary, at least with plain node.js executable
     }
     p0.stdout.on('data', (data) => {
       if (stdiopts & EXEC_OPTIONS.RETURN_STDOUT) {
-        std += data.toString();
+        stdout += data.toString();
       } else {
         t.log(data.toString());
       }
     });
     p0.stderr.on('data', (data) => {
-      std += data.toString();
+      stderr += data.toString();
     });
     p0.on('exit', (code) => {
       if (code === 0) {
         // if used in Visual Studio these are some debug outputs added to the output
-        std = std.replace(/Debugger attached./, '').trimLeft();
-        std = std.replace(/Waiting for the debugger to disconnect.../, '').trimRight();
-        resolve(std ?? undefined);
+        stdout = stdout.replace(/Debugger attached./, '').trimLeft();
+        stdout = stdout.replace(/Waiting for the debugger to disconnect.../, '').trimRight();
+        resolve(stdout ?? undefined);
       } else {
-        reject(Error(`Failed to execute ${command} ${args.join(' ')} with exit-code ${code}\n${std}`));
+        reject(Error(`Failed to execute ${command} ${args.join(' ')} with exit-code ${code}\n${stderr}`));
       }
     });
   });
